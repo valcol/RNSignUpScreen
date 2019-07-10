f0f13723a8d366ec2da80c2a0cce7a3ea8d35b30
Valcol <valcol.nc@gmail.com>
2019-07-10 21:58:16 +0200

    feat(signup): add basic form

--- app/screens/SignUp/component.js
+++ app/screens/SignUp/component.js
@@ -1,6 +1,60 @@
 import React from "react";
-import { Text } from "react-native";
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Button,
+    Left,
+    Body,
+    Icon,
+    Text,
+    Form,
+    Item,
+    Label,
+    Input
+} from "native-base";
 
-const SignUp = () => <Text>SignUp</Text>;
+const SignUp = () => {
+    const renderSMSCodeDigitField = () => (
+        <Item regular>
+            <Input keyboardType={"phone-pad"} maxLength={1} />
+        </Item>
+    );
+
+    return (
+        <Container>
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name="arrow-back" />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>Créez votre compte</Title>
+                </Body>
+            </Header>
+            <Content>
+                <Form>
+                    <Label>Numéro de téléphone</Label>
+                    <Item regular>
+                        <Input keyboardType={"phone-pad"} maxLength={10} />
+                    </Item>
+                    <Label>Code de confirmation reçu par SMS</Label>
+                    {renderSMSCodeDigitField()}
+                    {renderSMSCodeDigitField()}
+                    {renderSMSCodeDigitField()}
+                    {renderSMSCodeDigitField()}
+                    <Button rounded bordered>
+                        <Text>Renvoyer le SMS</Text>
+                    </Button>
+                    <Button rounded>
+                        <Text>Suivant</Text>
+                    </Button>
+                </Form>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;
