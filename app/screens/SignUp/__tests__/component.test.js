be65a2449a759ef7054eaee655c7cccc3eac36f1
Valcol <valcol.nc@gmail.com>
2019-07-11 23:12:13 +0200

    test(signup): add test cases

--- app/screens/SignUp/__tests__/component.test.js
+++ app/screens/SignUp/__tests__/component.test.js
@@ -7,3 +7,27 @@ it("renders correctly with no props", () => {
     const tree = renderer.create(<SignUp />).toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+it("renders phone number error message", () => {
+    const tree = renderer.create(<SignUp isCodeRequestOnError />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders SMS code form", () => {
+    const tree = renderer.create(<SignUp isCodeRequestOnSuccess />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders SMS code error message", () => {
+    const tree = renderer
+        .create(<SignUp isCodeRequestOnSuccess isCodeVerificationOnError />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders SMS code success", () => {
+    const tree = renderer
+        .create(<SignUp isCodeRequestOnSuccess isCodeVerificationOnSuccess />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
